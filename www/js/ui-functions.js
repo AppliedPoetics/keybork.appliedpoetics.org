c2d98beb2cfb7087e9e2ae2ee9f0c442d48cef1e
Douglas Luman <admin@appliedpoetics.org>
2020-03-20 12:34:55 -0400

    This one's for Jenni

--- www/js/ui-functions.js
+++ www/js/ui-functions.js
@@ -1,18 +1,25 @@
 $(document).ready(function(){
-    $('#loading img').css({marginLeft: ($(document).width() - 101)/2 + "px", 
-                           marginTop: ($(document).height()/2.5) +"px"});
-    $('.dropdown').on('mouseenter',function() {
-      $(this).css('background','rgba(3,166,150, 1)');
-      $(this).css('color','rgba(255,255,255,1)');
-    });
-    $('.dropdown').on('mouseleave',function() {
-      $(this).css('background','rgba(242,242,242, 1)');
-      $(this).css('color','rgba(34,2,0, 1)');
-    });
-    $('.drop-btn').on('mouseenter',function() {
-      $(this).siblings('.dropdown-content').css('display','');
-    });
-    $('.modal-window').on('focusout',function() {
-        $(this).hide();
-    });
+  $('#loading img').css({marginLeft: ($(document).width() - 101)/2 + "px", 
+                         marginTop: ($(document).height()/2.5) +"px"});
+  $('.dropdown').on('mouseenter',function() {
+    $(this).css('background','rgba(3,166,150, 1)');
+    $(this).css('color','rgba(255,255,255,1)');
+  });
+  $('.dropdown').on('mouseleave',function() {
+    $(this).css('background','rgba(242,242,242, 1)');
+    $(this).css('color','rgba(34,2,0, 1)');
+  });
+  $('.drop-btn').on('mouseenter',function() {
+    $(this).siblings('.dropdown-content').css('display','');
+  });
+  $('.modal-window').on('focusout',function() {
+    $(this).hide();
+  });
+  $('.drop-btn').on('click', function() {
+    var method = $(this).attr('data-method');
+    if ( method ) {
+      $('#' + method).show();
+      $('#' + method).focus();
+    }
+  });
 });
